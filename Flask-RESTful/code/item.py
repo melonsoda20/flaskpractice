106c83597ad7924575b4f3119a2401fbacfb621b
melonsoda20 <benedictusteddy200596@gmail.com>
2022-06-13 15:35:14 +0700

    Update code to retrieve items endpoint

--- Flask-RESTful/code/item.py
+++ Flask-RESTful/code/item.py
@@ -103,5 +103,21 @@ class Item(Resource):
         return updated_item
 
 class ItemList(Resource):
+    @classmethod
+    def get_items(cls):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM items"
+        result = cursor.execute(query)
+
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+        return items
+
     def get(self):
+        items = self.get_items()
         return {'items': items}
